3c905cadbefae6ca9d9195b21c44e65a808e9a67
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-23 22:30:25 +0530

    changed 4 files

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/pw-g5Gz9TjN1701F/scene.splinecode" />
+  );
+}
