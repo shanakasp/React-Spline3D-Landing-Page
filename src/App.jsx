b87dee753077b5b08ec42c59ff5e041326c669de
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-24 10:15:56 +0530

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,52 @@ function App() {
         >
           <img src={Logo} alt='logo'></img>
         </div>
+        <div className='navList'>
+          <ul>
+            <li>
+              <a href='/'>home</a>
+            </li>
+
+            <li>
+              <a href='/'>about</a>
+            </li>
+
+            <li>
+              <a href='/'>prooject</a>
+            </li>
+
+            <li>
+              <a href='/'>contact</a>
+            </li>
+
+          </ul>
+        </div>
+        <div className='burgerContainer'>
+          <div className='burgerTrigger'></div>
+          <div className='burgerMenu'></div>
+        </div>
       </div>
     </nav>
-    
+    <section > <div className='contents'>
+      <h1>Let's Build This <span>Awesome Project</span></h1>
+      <p>This is a sample paragraph.</p> </div>
+      <div className='buttons'>
+        <button className='one'>Get Started.</button>
+        <button className='two'>oour Story.</button>
+         
+      </div>
+    </section>
+    <footer>
+      <div className='contents'>
+        <ul>
+          <li><a href='/'><img src={Instagram} alt='instagram'></img></a></li>
+          <li><a href='/'><img src={Linkedin} alt='linkedin'></img></a></li>
+          <li><a href='/'><img src={Facebook} alt='facebook'></img></a></li>
+         
+        </ul>
+      </div>
+    </footer>
+
     </div> 
   );
 }
