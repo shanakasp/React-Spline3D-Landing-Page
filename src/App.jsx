2e1658e274b6927c37c2fe40ea360e6010ae8325
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-24 09:59:58 +0530

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import Spline from '@splinetool/react-spline';
+import "./styles/main.scss"
+import Logo from "./images/logo.png"
+import Instagram from "./images/instagram.svg"
+import Facebook from "./images/facebook.svg"
+import Linkedin from "./images/linkedin.svg"
 
-export default function App() {
+function App() {
   return (
-    <Spline scene="https://prod.spline.design/pw-g5Gz9TjN1701F/scene.splinecode" />
+
+    <div className='App'> <Spline scene="https://prod.spline.design/pw-g5Gz9TjN1701F/scene.splinecode" />
+    <nav>
+      <div className='itemContainer'>
+        <div className='logo'
+        >
+          <img src={Logo} alt='logo'></img>
+        </div>
+      </div>
+    </nav>
+    
+    </div> 
   );
 }
+
+export default App;
