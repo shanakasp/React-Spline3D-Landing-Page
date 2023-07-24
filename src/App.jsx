ffbb082aaae211d7abf80b54d38caa9f7088e838
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-24 10:22:47 +0530

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,95 @@
-import Spline from '@splinetool/react-spline';
-import "./styles/main.scss"
-import Logo from "./images/logo.png"
-import Instagram from "./images/instagram.svg"
-import Facebook from "./images/facebook.svg"
-import Linkedin from "./images/linkedin.svg"
+import "./styles/main.scss";
+import Spline from "@splinetool/react-spline";
+import Logo from "./images/logo.png";
+import Instagram from "./images/instagram.svg";
+import Facebook from "./images/facebook.svg";
+import Linkedin from "./images/linkedin.svg";
+import { useState } from "react";
 
 function App() {
-  return (
+    const [toggleMenu, setToggleMenu] = useState(true);
 
-    <div className='App'> <Spline scene="https://prod.spline.design/pw-g5Gz9TjN1701F/scene.splinecode" />
-    <nav>
-      <div className='itemContainer'>
-        <div className='logo'
-        >
-          <img src={Logo} alt='logo'></img>
+    const handleToggleMenu = () => {
+        setToggleMenu(!toggleMenu);
+    };
+
+    return (
+        <div className="App">
+           <Spline scene="https://prod.spline.design/pw-g5Gz9TjN1701F/scene.splinecode" />
+            <nav className={toggleMenu === true ? "" : "active"}>
+                <div className="itemsContainer">
+                    <div className="logo">
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className="navList">
+                        <ul>
+                            <li>
+                                <a href="/">home</a>
+                            </li>
+                            <li>
+                                <a href="/">about</a>
+                            </li>
+                            <li>
+                                <a href="/">projects</a>
+                            </li>
+                            <li>
+                                <a href="/">contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="burgerContainer">
+                        <div
+                            className="burgerTrigger"
+                            onClick={() => {
+                                handleToggleMenu();
+                            }}
+                        ></div>
+                        <div className="burgerMenu"></div>
+                    </div>
+                </div>
+            </nav>
+            <section>
+                <div className="itemsContainer">
+                    <div className="contents">
+                        <h1>
+                            Let's build <span>something epic.</span>
+                        </h1>
+                        <p>
+                           Sample text.
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button className="one">Get started.</button>
+                        <button className="two">Our story.</button>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <div className="contents">
+                    <ul>
+                        <li>
+                            <a href="/">
+                                <img src={Instagram} alt="instagram" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <img src={Linkedin} alt="linkedin" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <img src={Facebook} alt="facebook" />
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="creator">
+                        <p>React with Slipe3D</p>
+                    </div>
+                </div>
+            </footer>
         </div>
-        <div className='navList'>
-          <ul>
-            <li>
-              <a href='/'>home</a>
-            </li>
-
-            <li>
-              <a href='/'>about</a>
-            </li>
-
-            <li>
-              <a href='/'>prooject</a>
-            </li>
-
-            <li>
-              <a href='/'>contact</a>
-            </li>
-
-          </ul>
-        </div>
-        <div className='burgerContainer'>
-          <div className='burgerTrigger'></div>
-          <div className='burgerMenu'></div>
-        </div>
-      </div>
-    </nav>
-    <section > <div className='contents'>
-      <h1>Let's Build This <span>Awesome Project</span></h1>
-      <p>This is a sample paragraph.</p> </div>
-      <div className='buttons'>
-        <button className='one'>Get Started.</button>
-        <button className='two'>oour Story.</button>
-         
-      </div>
-    </section>
-    <footer>
-      <div className='contents'>
-        <ul>
-          <li><a href='/'><img src={Instagram} alt='instagram'></img></a></li>
-          <li><a href='/'><img src={Linkedin} alt='linkedin'></img></a></li>
-          <li><a href='/'><img src={Facebook} alt='facebook'></img></a></li>
-        </ul>
-        <div className='creator'>
-          <p>Done with React and Slipe3D</p>
-        </div>
-      </div>
-    </footer>
-
-    </div> 
-  );
+    );
 }
 
 export default App;
